f0706513d3c60ec99c4ff1bb42a34ef516c677a0
Michael Hirsch <scivision@users.noreply.github.com>
2025-08-07 22:41:36 -0400

    use GKlib as Git submodule, to facilitate offline use

---
+++ cmake/GitSubmodule.cmake
@@ -0,0 +1,38 @@
+function(git_submodule submod_dir)
+# get/update Git submodule directory to CMake, assuming the
+# Git submodule directory is a CMake project.
+
+if(EXISTS ${submod_dir}/CMakeLists.txt)
+  return()
+endif()
+
+cmake_path(GET submod_dir STEM submod_name)
+
+find_package(Git REQUIRED)
+
+set(CMAKE_EXECUTE_PROCESS_COMMAND_ECHO STDOUT)
+
+# EXISTS, do not use IS_DIRECTORY as in submodule .git is a file not a directory
+if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
+  message(DEBUG "${PROJECT_SOURCE_DIR} is a Git repository, updating submodule ${submod_dir}")
+  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ${submod_dir}
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND_ERROR_IS_FATAL ANY
+    )
+else()
+  message(DEBUG "${PROJECT_SOURCE_DIR} is from an archive or otherwise not a Git repository. Getting .gitmodules info to clone")
+  execute_process(COMMAND ${GIT_EXECUTABLE} config --file ${PROJECT_SOURCE_DIR}/.gitmodules --get submodule.${submod_name}.url
+                  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                  OUTPUT_VARIABLE submod_url
+                  OUTPUT_STRIP_TRAILING_WHITESPACE
+                  COMMAND_ERROR_IS_FATAL ANY
+                  )
+  # Some platforms like Windows, Git might refuse to clone into a totally empty existing directory
+  # this is a known technique, to "git clone" from that empty submod_dir directory to make it work.
+  execute_process(COMMAND ${GIT_EXECUTABLE} clone ${submod_url} ${submod_dir}
+                  WORKING_DIRECTORY ${submod_dir}
+                  COMMAND_ERROR_IS_FATAL ANY
+                  )
+endif()
+
+endfunction()
